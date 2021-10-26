ee8c1b072c4e77f9c879947cda3f1ef973c96c4f
Devon Dunham <itisdevondunham@gmail.com>
2021-10-26 12:15:48 -0400

    Pause Menu: Finished, Needs to be placed in all levels except 1 & 7

---
+++ JuniorsPeriod5Capstone/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pauseMenuCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenuCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Pause();
+            pauseMenuCanvas.SetActive(true);
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pauseMenuCanvas.SetActive(false);
+    }
+
+    public void Menu()
+    {
+        Debug.Log("Go to Menu");
+    }
+
+}
