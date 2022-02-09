9f7ffe8744b31e10e8a957832c0696ae57009b22
Devon Dunham <77740379+devonDunham@users.noreply.github.com>
2022-02-09 12:15:58 -0500

    done: scene transitions

--- JuniorsPeriod5Capstone/Assets/Scripts/SceneTransition.cs
+++ JuniorsPeriod5Capstone/Assets/Scripts/SceneTransition.cs
@@ -2,21 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneTransition : MonoBehaviour
 {
-    public string sceneToGoTo;
-
+    public RectTransform fader;
+    public string sceneToLoad;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        StartCoroutine(FadeInCo());
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator FadeInCo()
     {
+        yield return new WaitForSeconds(0.05f);
 
+        fader.gameObject.SetActive(true);
+        LeanTween.scale(fader, new Vector3(1, 1, 1), 0);
+        LeanTween.scale(fader, Vector3.zero, 0.7f).setOnComplete(() =>
+        {
+            fader.gameObject.SetActive(false);
+        });
+    }
+
+    public IEnumerator OpenSceneCo()
+    {
+        PlayerMovement.instance.canMove = false;
+        PlayerMovement.instance.speed = 0f;
+        PlayerMovement.instance.moveInput = 0f;
+
+        yield return new WaitForSeconds(2f);
+
+        fader.gameObject.SetActive(true);
+        LeanTween.scale(fader, Vector3.zero, 0f);
+        LeanTween.scale(fader, new Vector3(1, 1, 1), 0.7f).setOnComplete(() =>
+        {
+            if (sceneToLoad != "")
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+            else
+            {
+                Debug.Log("Scene to load is empty on");
+                return;
+            }
+        });
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(OpenSceneCo());
+        }
     }
 }
