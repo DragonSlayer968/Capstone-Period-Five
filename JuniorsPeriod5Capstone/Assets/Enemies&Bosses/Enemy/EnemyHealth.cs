b600180359650778cfbc9f5ea2d102d3b6c80a49
Aidan Lauser <lauser.aidan@gmail.com>
2022-02-09 14:59:42 -0500

    Trying to make enemy patrol, made hit

--- JuniorsPeriod5Capstone/Assets/Enemies&Bosses/Enemy/EnemyHealth.cs
+++ JuniorsPeriod5Capstone/Assets/Enemies&Bosses/Enemy/EnemyHealth.cs
@@ -5,22 +5,42 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     public float enemyHealth;
+    public bool invinsible;
+    public float IV, IVOrig;
+
+    public Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(invinsible == true)
+        {
+            IV -= Time.deltaTime;
+            if(IV <= 0)
+            {
+                IV = IVOrig;
+                invinsible = false;
+            }
+        }
     }
 
     public void Hit(float damage)
     {
-        enemyHealth -= damage;
+        
+        if(invinsible == false)
+        {
+            anim.SetTrigger("Hit");
+
+            enemyHealth -= damage;
+            invinsible = true;
+        }
+        
     }
 
 }
---
+++ JuniorsPeriod5Capstone/Assets/Enemies&Bosses/ExtraEnemyScripts/EnemyPatrol.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public bool patrol;
+    public int patrolValue;
+
+    public Vector3[] patrolPoints;
+
+    public float speedPatrol;
+    public Rigidbody2D rb;
+    public Animator anim;
+    public SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(patrol = true)
+        {
+            anim.SetBool("Walking", true);
+            if (patrolValue == 0) // left
+            {
+                if(transform.position.x <= patrolPoints[patrolValue].x)
+                {
+                    patrolValue = 1;
+                }
+
+                else
+                {
+                    sprite.flipX = false;
+                    rb.velocity = new Vector2(-speedPatrol, 0);
+                }
+
+            }
+
+            else //right
+            {
+                if (transform.position.x >= patrolPoints[patrolValue].x)
+                {
+                    patrolValue = 0;
+                }
+
+                else
+                {
+                    sprite.flipX = true;
+                    rb.velocity = new Vector2(speedPatrol, 0);
+                }
+            }
+        }
+
+
+    }
+}
