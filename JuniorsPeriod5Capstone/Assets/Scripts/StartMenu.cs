f06842a09040124ded3e0f7b7c447c73bf9ffbb5
StandingJon <jonathanowens3000@gmail.com>
2022-02-10 22:55:47 -0500

    Start and Credits Menus
    Added the start and credits menus to the game

---
+++ JuniorsPeriod5Capstone/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void PlayTutorial ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void PlayFirstLevel ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+    public void QuitGame ()
+    {
+        Debug.Log("The Quit Button has been pressed");
+        Application.Quit();
+    }
+    public void PlayCredits ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void PlayStartMenu ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
