c2299e9b7dca32d6b7aca47911309f09c850ff63
ConnorDaland <88779587+ConnorDaland@users.noreply.github.com>
2022-03-18 16:18:54 -0400

    ongoing: plaza and background objects

--- JuniorsPeriod5Capstone/Assets/Scripts/SceneTransition.cs
+++ JuniorsPeriod5Capstone/Assets/Scripts/SceneTransition.cs
@@ -33,7 +33,7 @@ public class SceneTransition : MonoBehaviour
         PlayerMovement.instance.speed = 0f;
         PlayerMovement.instance.moveInput = 0f;
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(.5f);
 
         fader.gameObject.SetActive(true);
         LeanTween.scale(fader, Vector3.zero, 0f);
