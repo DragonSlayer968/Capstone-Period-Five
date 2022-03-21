c553a0c2fc8f296cc4c024519fda605ac81f1e43
ConnorDaland <88779587+ConnorDaland@users.noreply.github.com>
2022-03-21 16:51:25 -0400

    level 3 and 4 enemies, including boss

---
+++ JuniorsPeriod5Capstone/Assets/Scripts/BossSummoner.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSummoner : MonoBehaviour
+{
+    public GameObject boss;
+    public GameObject obj;
+
+    public bool objActivate;
+    public bool objDeactivate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!boss)
+        {
+            if (objActivate)
+            {
+                obj.SetActive(true);
+            }
+            if (objDeactivate)
+            {
+                obj.SetActive(false);
+            }
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            boss.SetActive(true);
+            if (objActivate)
+            {
+                obj.SetActive(false);
+            }
+            if (objDeactivate)
+            {
+                obj.SetActive(true);
+            }
+        }
+    }
+}
